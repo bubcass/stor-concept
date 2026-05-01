c687075bb562059d0a3dea413c1aac97dc0951cd
bubcass <81823823+bubcass@users.noreply.github.com>
2026-05-01 13:43:03 +0100

    Improving lock screen artwork

--- src/lib/components/story/StoryToolbar.svelte
+++ src/lib/components/story/StoryToolbar.svelte
@@ -80,6 +80,11 @@
       artist: 'Inside Parliament',
       album: 'Inside Parliament',
       artwork: [
+        {
+          src: `${base}/icons/inside-parliament-lockup-square.svg`,
+          sizes: '512x512',
+          type: 'image/svg+xml'
+        },
         { src: `${base}/icons/icon-192.png`, sizes: '192x192', type: 'image/png' },
         { src: `${base}/icons/icon-512.png`, sizes: '512x512', type: 'image/png' }
       ]
