9de7f6fc3977a30c83007c377e3f15aaf81b101f
bubcass <81823823+bubcass@users.noreply.github.com>
2026-05-18 18:01:05 +0100

    Bug fix

--- src/routes/proof-of-concept/+page.ts
+++ src/routes/proof-of-concept/+page.ts
@@ -1,5 +1,6 @@
+import { base } from "$app/paths";
 import { redirect } from "@sveltejs/kit";
 
 export function load() {
-    redirect(308, "/proof-of-concept/committees/");
+    redirect(308, `${base}/proof-of-concept/committees/`);
 }
